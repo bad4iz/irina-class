fbead0cc5240cb935c314955b830cc65ae0bc97a
zlatta <violetta@plotnikovs.com>
2020-12-17 22:53:54 +0400

    добавила картинки

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\www\irina-class\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\www\irina-class\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\Users\Виолетта\www\irina-class\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Виолетта\www\irina-class\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
