a7c86ffe9a3d9b751d215ce205085945ba61e201
bad4iz <develop@bad4iz.ru>
2021-01-17 12:05:40 +0400

    h1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
     ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {    },
+    extend: {},
+      fontFamily: {
+          'neucha': ['neucha', 'Georgia', 'system-ui'],
+      }
   },
   variants: {
     extend: {},
